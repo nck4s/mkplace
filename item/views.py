0ce8d1f9c83dbc9a746a5258f5572b1c6ff6cfc4
nckss <nn.2nn@mail.ru>
2023-08-31 22:54:13 +1000

    fix detail_item mistake

--- item/views.py
+++ item/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
 # Create your views here.
+def item_detail(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    return render(request, 'item/detail.html', {'item': item})
