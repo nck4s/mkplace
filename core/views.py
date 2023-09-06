def9ff50c2ec34324bcf3d5e6000e5dae6c0d90a
nckss <nn.2nn@mail.ru>
2023-09-06 11:45:15 +1000

    about done

--- core/views.py
+++ core/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
+
 from item.models import Category, Item
+
 from .forms import SignupForm
 
-# Create your views here.
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
     return render(request, 'core/index.html', {
         'categories': categories,
-        'items': items, 
+        'items': items,
     })
 
 def contact(request):
@@ -23,8 +24,9 @@ def signup(request):
             form.save()
 
             return redirect('/login/')
-        else: 
-            form = SignupForm()
+    else:
+        form = SignupForm()
 
-    
-    return render(request, 'core/signup.html', {'form': form})
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
